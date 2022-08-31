94a2e7fdd3890c3c80dd653f7b840bdc85c19229
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-08-31 11:57:00 +0000

    モデル(Address)作成

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,6 +4,7 @@ class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   before_action :configure_permitted_parameters , if: :devise_controller?
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -26,6 +27,22 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  # 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+    ## 【処理内容3】
+    if @customer.is_deleted?(params[:customer][:is_deleted])
+      if true then
+      redirect_to new_customer_registration
+      end
+    end
+    end
 
   def configure_permitted_parameters
    	  devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
--- db/migrate/20220830143609_devise_create_customers.rb
+++ db/migrate/20220830143609_devise_create_customers.rb
@@ -15,7 +15,6 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       t.string :postal_code, null: false
       t.string :address, null: false
       t.string :telephone_number, null: false
-      t.boolean :is_deleted, null: false, default: false
 
       ## Recoverable
       t.string   :reset_password_token
