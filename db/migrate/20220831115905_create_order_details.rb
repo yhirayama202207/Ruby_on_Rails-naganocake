2790264abeebc59ab5c270601d417ffb2a852f84
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-08-31 12:09:11 +0000

    各テーブルに必要な情報入力

--- db/migrate/20220831115905_create_order_details.rb
+++ db/migrate/20220831115905_create_order_details.rb
@@ -1,6 +1,13 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
+      t.integer :item_id, null: false
+      t.integer :order_id, null: false
+      t.integer :price, null: false
+      t.integer :amount, null: false
+      t.integer :making_status, null: false, default: 0
+      
+      enum making_status: { can_not_product: 0, not_yet_started: 1, in_progress: 2, done: 3 }
 
       t.timestamps
     end
--- db/migrate/20220831120014_create_orders.rb
+++ db/migrate/20220831120014_create_orders.rb
@@ -1,6 +1,17 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.integer :customer_id, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :name, null: false
+      t.integer :shipping_cost, null: false
+      t.integer :total_payment, null: false
+      t.integer :payment_method, null: false, default: 0
+      t.integer :status, null: false, default: 0
+      
+      enum payment_method: { credit_card: 0, transfer: 1 }
+      enum status: { not_yet_paid: 0, paid: 1, in_production: 2, shipping_preparation: 3, sent: 4 }
 
       t.timestamps
     end
