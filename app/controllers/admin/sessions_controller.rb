f7e2d7d71e688a8259aa98c7b117fac5e7c5c397
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-08-31 09:06:17 +0000

    viewの作成(public/顧客用)

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,6 +2,8 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  
+  before_action :configure_permitted_parameters , if: :devise_controller?
 
   # GET /resource/sign_in
   # def new
@@ -18,10 +20,14 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def configure_permitted_parameters
+   	  devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+  end
 end
--- db/migrate/20220830143609_devise_create_customers.rb
+++ db/migrate/20220830143609_devise_create_customers.rb
@@ -7,12 +7,23 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## Database
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :telephone_number, null: false
+      t.boolean :is_deleted, null: false, default: false
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
+      t.datetime :remember_updated_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
