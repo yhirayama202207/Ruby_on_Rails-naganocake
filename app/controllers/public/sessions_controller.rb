bad237142ec657d15ab661d135664fa97da40acb
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-08-31 10:00:02 +0000

    ルーティングの編集

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  
+
   before_action :configure_permitted_parameters , if: :devise_controller?
 
   # GET /resource/sign_in
@@ -26,7 +26,7 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
+
   def configure_permitted_parameters
    	  devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
   end
