8f3a795f9493aad1d512a892a969b4448a3197a4
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-04 15:43:51 +0000

    Routing設定（for public)

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,33 @@ Rails.application.routes.draw do
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
-    sessions: 'public/sessions'
+    sessions: 'public/sessions',
   }
+
+  get "/" => "public/homes#top"
+  get "about" => "public/homes#about"
+  get "items" => "public/items#index"
+  get "items/:id" => "public/items#show"
+  get "customers" => "public/customers#show"
+  get "customers/edit" => "public/customers#edit"
+  get "customers/unsubscribe/confirm" => "public/customers#confirm"
+  get "cart_items" => "public/cart_items#index"
+  get "orders/new" => "public/orders#new"
+  get "orders/confirm" => "public/orders#confirm"
+  get "orders/complete" => "public/orders#complete"
+  get "orders/index" => "public/orders#index"
+  get "orders/:id" => "public/orders#show"
+  get "addresses" => "public/addresses#index"
+  get "addresses/:id/edit" => "public/addresses#edit"
 
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    get 'items' => 'admin/items#index'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
