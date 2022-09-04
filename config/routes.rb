d3b663bfb1aadf255ca0bae5feb7ad7a80c67e03
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-04 16:07:51 +0000

    Routing設定（for admin追記)

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
-    resources :items
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
