cd2676b4a3a58b78254f6853b465cb7ae87d47a2
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-05 04:39:41 +0000

    ログインに関するroutingの設定（管理者側）

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -22,12 +22,20 @@ before_action :configure_permitted_parameters , if: :devise_controller?
 
   protected
 
+  def after_sign_in_path_for(resource)
+    admin_order(resource)
+  end
+
+  def after_sign_out_path_for
+    new_admin_session
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
   def configure_permitted_parameters
-   	  devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+   	  devise_parameter_sanitizer.permit(:sign_up,keys:[:email, :encrypted_password])
   end
 end
