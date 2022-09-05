5d7c9328ca3f7dcb14600ca30e91dbbeaad5097b
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-05 04:30:14 +0000

    管理者ログイン用メールアドレス/パスワード設定（seeds.rb)

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  before_action :configure_permitted_parameters , if: :devise_controller?
+before_action :configure_permitted_parameters , if: :devise_controller?
 
   # GET /resource/sign_in
   # def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+
+Admin.create!(
+  email: 'admin@admin.com',
+  encrypted_password: 'admin'
+)
