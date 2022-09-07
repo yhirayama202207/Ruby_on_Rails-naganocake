cf5a662cd21709de8e272221cfe5a1af4ea106fd
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-07 02:39:19 +0000

    views/admin/customers/showの編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,11 +1,12 @@
 class Admin::CustomersController < ApplicationController
 
   def index
-    @customers = Customer.all
+    @customers = Customer.all.page(params[:page])
   end
 
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
