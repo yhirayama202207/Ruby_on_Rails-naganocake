c0593fc764fec9e4dd06fb61d6ced3d00271d374
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-07 03:08:08 +0000

    views/admin/customers/showとedit,customers_controllerの編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,9 +10,22 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "顧客情報の更新が完了しました"
+      redirect_to admin_customer_path(@customer.id)
+    end
+  end
+
+  #投稿データのストロングパラメータ
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number,:email, :is_deleted)
   end
 
 end
