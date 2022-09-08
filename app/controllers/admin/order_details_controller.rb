34e430944f4cf94fe2975eefd0f1df364e9e1769
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-08 08:37:48 +0000

    views/public/orders/completeの編集

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,6 +1,7 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
+    
   end
 
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,10 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @order = Order.find(params[:id])
+    @customer = @order.customer
+    @order_details = Order_details.find(params[:id])
+    @item = @order_details.item
   end
 
   def update
