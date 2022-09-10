fdca9ca87c94108084238648c7a9493e2e9ab03d
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-10 09:51:05 +0000

    views/admin/items/index,showとadmin/items_controllerの編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -41,7 +41,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :is_active)
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
   end
 
 end
