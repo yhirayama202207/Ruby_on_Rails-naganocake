69bb0093579a5b90ee367e41b16bd06aa34a2a4f
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-13 12:56:20 +0000

    public側商品詳細画面／カート画面／トップページ／新規登録画面の編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,14 @@
 class Public::CartItemsController < ApplicationController
 
   def index
+    @cart_items = current_customer.cart_items
+    @total = 0
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to :cart_items
   end
 
   def destroy
@@ -13,6 +18,17 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = current_customer.cart_items.build(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+      if cart_item.item_id == @cart_item.item_id
+        new_amount = cart_item.amount + @cart_item.amount.to_i
+        cart_item.update_attribute(:amount, new_amount)
+        @cart_item.delete
+      end
+    end
+    @cart_item.save
+    redirect_to :cart_items
   end
 
   private
