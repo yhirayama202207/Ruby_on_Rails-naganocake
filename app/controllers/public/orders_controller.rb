77be113b5eef5373f84f4afd41c58b8d7f72a8fc
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-09-27 17:26:25 +0000

    2022.09.28_2:26

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,15 +6,22 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    if params[:order][:addresses] == "0"
-      @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.name = current_customer.last_name + current_customer.first_name
-    elsif params[:order][:addresses] == "1"
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
+    # if params[:order][:addresses] == "0"
+    #   @order.postal_code = current_customer.postal_code
+    #   @order.address = current_customer.address
+    #   @order.name = current_customer.last_name + current_customer.first_name
+    # elsif params[:order][:addresses] == "1"
 
-    elsif params[:order][:addresses] == "2"
+    # elsif params[:order][:addresses] == "2"
 
-    end
+    # end
+    @cart_items = current_customer.cart_items
+    @order.shipping_cost = 800
+    @total = 0
   end
 
   def complete
@@ -41,7 +48,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :shipping_cost)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 
 end
